e8116f5ab0a722625559240eea2431ce8820dc2c
Richard Li <lifuzu@gmail.com>
2019-01-21 11:38:08 -0800

    Create tools for DB deployment

---
+++ tools/db/create_database.sql
@@ -0,0 +1,12 @@
+-- Database: django.retries
+
+-- DROP DATABASE "django.retries";
+
+CREATE DATABASE "django.retries"
+    WITH
+    OWNER = rlee
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
