698b7dfb86dadf5c48066239c15b2c5ce7036dd0
Richard Li <lifuzu@gmail.com>
2019-01-21 19:50:38 -0800

    Creating Django rest framework hooks for tasks

---
+++ retries/signals.py
@@ -0,0 +1,3 @@
+
+def create_retry_task(sender, instance, created, **kwargs):
+    print("retry created!")
--- retries/views.py
+++ retries/views.py
@@ -2,6 +2,22 @@ from retries.models import Retry
 from retries.serializers import RetrySerializer
 from rest_framework import generics
 
+from retries.signals import create_retry_task
+
+
 class RetryListCreate(generics.ListCreateAPIView):
     queryset = Retry.objects.all()
     serializer_class = RetrySerializer
+
+    # Perform a custom pre/post-save action
+    # Reference:
+    # https://www.django-rest-framework.org/api-guide/generic-views/
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        # Perform a custom post-save action after a new data inserted.
+        create_retry_task(sender=self, instance=instance, created=True)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        # Perform a custom post-save action after a data updated.
+        create_retry_task(sender=self, instance=instance, created=True)
